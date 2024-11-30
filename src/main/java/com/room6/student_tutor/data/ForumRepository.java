b3fe69032ebf2ab825236ff08a8c8af5c464d2c7
bangoldx <bangold@proton.me>
2024-11-30 00:32:53 -0600

    upsteam

--- src/main/java/com/room6/student_tutor/data/ForumRepository.java
+++ src/main/java/com/room6/student_tutor/data/ForumRepository.java
@@ -1,7 +1,7 @@
 package com.room6.student_tutor.data;
 
 import com.room6.student_tutor.models.Forum;
-import com.room6.student_tutor.models.Student;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
